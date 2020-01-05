d003d982213a964449aa4bf5619e77c5541759e1
Lucas Eduardo <lucased78@hotmail.com>
2020-01-05 09:37:25 -0300

    setting up text field

--- Controller/WeatherViewController.swift
+++ Controller/WeatherViewController.swift
@@ -8,14 +8,46 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
-    @IBOutlet weak var conditionImageView: UIImageView!
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var weatherCondition: UIImageView!
+    
     @IBOutlet weak var temperatureLabel: UILabel!
+    
     @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(textField.text)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text == ""{
+            textField.placeholder = "type something"
+            textField.textColor = UIColor.systemRed
+        }
+        
+        return textField.text == ""
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(textField.text)
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
